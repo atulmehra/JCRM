59b1c6bf9cc71bf92e4952aec8f6827cf65a816c
Atul Mehra <eng.amehra@gmail.com>
2020-05-16 19:05:27 +0530

    Updates

---
+++ precache-manifest.569b2544055cf776110a861ed5d580ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b402401e5e71097ea227ff60907d25a2",
+    "url": "/JCRM/index.html"
+  },
+  {
+    "revision": "475c9668667f3952debc",
+    "url": "/JCRM/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8b7b0ab842e998d78f9",
+    "url": "/JCRM/static/js/2.d71ff955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JCRM/static/js/2.d71ff955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475c9668667f3952debc",
+    "url": "/JCRM/static/js/main.13387477.chunk.js"
+  },
+  {
+    "revision": "e93506d96b4dc2e6d736",
+    "url": "/JCRM/static/js/runtime-main.a0417295.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JCRM/static/media/logo.5d5d9eef.svg"
+  }
+]);
